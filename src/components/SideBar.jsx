737800ee3bdba2e71e6ee3a66141005bef89b935
Aashutosh Gupta <114292967+Scripting-Aashutosh@users.noreply.github.com>
2024-08-27 18:22:43 +0530

    Update SideBar.jsx

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 //import Link
-import { Link } from 'react-router-dom';
+import { Link } from '`react-router-dom`';
 
 //import icons
 import { IoMdArrowForward } from 'react-icons/io';
