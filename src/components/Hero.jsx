8abb42d369b54954f344a23d328fe268344def9a
Aashutosh Gupta <114292967+Scripting-Aashutosh@users.noreply.github.com>
2024-08-27 18:22:09 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import Model from '../assets/model.png';
 
-import { Link } from 'react-router-dom';
+import { Link } from '`react-router-dom`';
 
 const Hero = () => {
   return (
