eaa9aeeebb65d0acb91a81d95e248a09c4249053
Aashutosh Gupta <114292967+Scripting-Aashutosh@users.noreply.github.com>
2024-08-27 18:22:29 +0530

    Update Product.jsx

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react'
 
 //import link
-import { Link } from 'react-router-dom';
+import { Link } from '`react-router-dom`';
 
 //import icons
 import { BsPlus, BsEyeFill } from 'react-icons/bs';
